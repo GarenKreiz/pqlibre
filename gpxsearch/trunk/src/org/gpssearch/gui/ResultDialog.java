b9eb07be4e3755d45a31a312d8582c1f22ac9152
smobailey <smobailey@gmail.com>
2012-09-06 20:02:16 +0000

    Display map of search result.

--- gpxsearch/trunk/src/org/gpssearch/gui/ResultDialog.java
+++ gpxsearch/trunk/src/org/gpssearch/gui/ResultDialog.java
@@ -2,7 +2,9 @@ package org.gpssearch.gui;
 
 import java.awt.Desktop;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.util.List;
 
 import org.eclipse.swt.SWT;
@@ -16,15 +18,19 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 import org.geoscrape.Cache;
+import org.geoscrape.CacheType;
+import org.geoscrape.Location;
 import org.gpssearch.GpxWriter;
 import org.gpssearch.UserIdManager;
 
 /**
- * Displays the result of a search operation and presents the user with options of what to do.
- *  
+ * Displays the result of a search operation and presents the user with options
+ * of what to do.
+ * 
  */
 public class ResultDialog extends Dialog
 {
+	private static final int URI_SIZE_LIMIT = 8192;
 
 	protected Object result;
 	protected Shell shlSearchResults;
@@ -34,6 +40,7 @@ public class ResultDialog extends Dialog
 	private List<Cache> caches;
 	private UserIdManager idManager;
 	private Desktop desktop;
+	private Button btnDisplayMap;
 
 	/**
 	 * Create the dialog.
@@ -65,9 +72,10 @@ public class ResultDialog extends Dialog
 		if (caches.size() > 0)
 		{
 			btnSaveFile.setEnabled(true);
-			if(desktop!=null)
+			if (desktop != null)
 			{
 				btnOpenInBrowser.setEnabled(true);
+				btnDisplayMap.setEnabled(true);
 			}
 			if (caches.size() == 1)
 			{
@@ -174,7 +182,7 @@ public class ResultDialog extends Dialog
 				}
 				if (open)
 				{
-					for(Cache c:caches)
+					for (Cache c : caches)
 					{
 						try
 						{
@@ -196,5 +204,165 @@ public class ResultDialog extends Dialog
 		resultText.setBounds(42, 45, 393, 15);
 		resultText.setText("No caches found.");
 
+		btnDisplayMap = new Button(shlSearchResults, SWT.NONE);
+		btnDisplayMap.addSelectionListener(new SelectionAdapter()
+		{
+			@Override
+			public void widgetSelected(SelectionEvent e)
+			{
+				displayMap();
+			}
+		});
+		btnDisplayMap.setEnabled(false);
+		btnDisplayMap.setBounds(38, 174, 101, 24);
+		btnDisplayMap.setText("Show on map");
+
+	}
+
+	/**
+	 * Show all the caches in the display map.
+	 */
+	protected void displayMap()
+	{
+		try
+		{
+			String newline = URLEncoder.encode("\n", "UTF-8");
+			StringBuilder url = new StringBuilder(
+					"http://www.gpsvisualizer.com/map_input?data=name,description,latitude,longitude,icon_size,symbol");
+			int count = 0;
+			for (Cache c : caches)
+			{
+				StringBuilder tmp = new StringBuilder(newline);
+				tmp.append(escape(c.getName()));
+				tmp.append(",");
+				StringBuilder desc = new StringBuilder();
+				desc.append("<a href=\"http://coord.info/");
+				desc.append(c.getCacheCode());
+				desc.append("\" target=\"_blank\">");
+				desc.append(c.getCacheCode());
+				desc.append("</a><br>");
+				desc.append(c.getCacheType());
+				desc.append("<br>D/T: ");
+				desc.append(c.getDifficultyRating());
+				desc.append("/");
+				desc.append(c.getTerrainRating());
+				desc.append("<br>Size: ");
+				desc.append(c.getCacheSize().toString());
+				tmp.append(escape(desc.toString()));
+				tmp.append(",");
+				Location loc = c.getLocation();
+				tmp.append(loc.getLatitude().toDecimalString());
+				tmp.append(",");
+				tmp.append(loc.getLongitude().toDecimalString());
+				tmp.append(",16x16,");
+				tmp.append(getIconUrlString(c.getCacheType()));
+				if(url.length()+tmp.length()>=URI_SIZE_LIMIT)
+				{
+					MessageBox messageBox = new MessageBox(shlSearchResults, SWT.ICON_INFORMATION | SWT.OK );
+					messageBox.setMessage("We're sorry, but gpsvisualizer.com can't display that many caches at once.\n" +
+							"We'll display the closest "+count+" caches instead.");
+					messageBox.setText("Opening a large number of caches.");
+					messageBox.open();
+					break;
+				}
+				url.append(tmp);
+				count++;
+			}
+			desktop.browse(new URI(url.toString()));
+
+		}
+		catch (Exception e1)
+		{
+			e1.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param cacheType
+	 * @return
+	 */
+	private String getIconUrlString(CacheType cacheType)
+	{
+		StringBuilder res = new StringBuilder("http://www.geocaching.com/images/wpttypes/sm/");
+		res.append(getId(cacheType));
+		res.append(".gif");
+		return res.toString();
+	}
+
+	/**
+	 * @param cacheType
+	 * @return
+	 */
+	private Object getId(CacheType cacheType)
+	{
+		String res = "";
+		switch (cacheType)
+		{
+			case TRADITIONAL:
+				res = "2";
+				break;
+			case MULTI:
+				res = "3";
+				break;
+			case VIRTUAL:
+				res = "4";
+				break;
+			case LETTERBOX:
+				res = "5";
+				break;
+			case EVENT:
+				res = "6";
+				break;
+			case MYSTERY:
+				res = "8";
+				break;
+			case PROJECTAPE1:
+			case PROJECTAPE2:
+				res = "9";
+				break;
+			case WEBCAM:
+				res = "11";
+				break;
+			case CITO:
+				res = "13";
+				break;
+			case BENCHMARK:
+				res = "27";
+				break;
+			case WHERIGO:
+				res = "1858";
+				break;
+			case EARTH_CACHE:
+				res = "earthcache";
+				break;
+			case LOST_AND_FOUND_EVENT:
+				res = "3653";
+				break;
+			case MEGA_EVENT:
+				res="453";
+				break;
+			case GPSADVENTURE:
+				res = "1304";
+				break;
+			default:
+				//use traditional as the default
+				res = "2";
+				break;
+
+		}
+		return res;
+	}
+
+	/**
+	 * Format the string for inclusion in gpsvisualiser.com's URL format.
+	 * 
+	 * @param string
+	 * @return
+	 * @throws UnsupportedEncodingException
+	 */
+	private Object escape(String string) throws UnsupportedEncodingException
+	{
+		string = URLEncoder.encode(string, "UTF-8");
+		return string;
 	}
 }
